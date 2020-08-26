087fdc72ec76e235acfb7131a280951213f88879
Colin Walters <walters@verbum.org>
2020-08-26 14:10:50 +0000

    Remove non-bash macros
    I'm not personally interested in the Windows (or non-bash) cases
    right now; we can reintroduce them later.

--- src/macros.rs
+++ src/macros.rs
@@ -1,33 +1,3 @@
-/// Create a [`Command`] object by parsing an argument string.
-///
-/// [`Command`]: https://doc.rust-lang.org/std/process/struct.Command.html
-#[macro_export]
-macro_rules! command {
-    ($fmt:expr) => ( command!($fmt,) );
-    ($fmt:expr, $( $id:ident = $value:expr ),* $(,)*) => (
-        {
-            $crate::internal_sh_inline_commandify(
-                format!($fmt, $( $id = $crate::command_arg(&$value) ),*)
-            )
-        }
-    );
-}
-
-/// Parse and execute a single command, returning an error if it
-/// exits unsuccessfully.  This is intended as a convenience function;
-/// if for example you might want to change behavior based on specific
-/// exit codes, it's recommended to use `command()` instead.
-#[macro_export]
-macro_rules! execute {
-    ($fmt:expr) => ( execute!($fmt,) );
-    ($fmt:expr, $( $id:ident = $value:expr ),* $(,)*) => (
-        {
-            use $crate::{CommandSpecExt};
-            $crate::internal_sh_inline_execute($crate::command!($fmt, $( $id = $value ),*).unwrap())
-        }
-    );
-}
-
 /// Create a [`Command`] object that will execute a fragment of (Bash) shell script
 /// in "strict mode", i.e. with `set -euo pipefail`.
 ///
--- tests/all.rs
+++ tests/all.rs
@@ -1,75 +1,55 @@
 extern crate sh_inline;
-use sh_inline::command;
+use sh_inline::{bash, bash_command};
+use std::ffi::OsStr;
+use std::os::unix::ffi::OsStrExt;
+use std::path::Path;
 
-#[cfg(not(windows))]
-mod sh {
-    use std::path::Path;
-    use sh_inline::{bash, bash_command};
-    use std::ffi::OsStr;
-    use std::os::unix::ffi::OsStrExt;
-
-    #[test]
-    fn sh_exit() {
-        assert!(bash!(r"exit {a}", a = 42).is_err())
-    }
-
-    #[test]
-    fn sh_echo1() {
-        let res = bash_command!(r"A={a}; echo $A", a = "SENTINEL")
-            .unwrap()
-            .output()
-            .unwrap();
-        assert_eq!(res.stdout, b"SENTINEL\n");
-    }
-
-    #[test]
-    fn sh_echo2() {
-        let res = bash_command!(r"A={a}; echo $A", a = "SENTINEL",)
-            .unwrap()
-            .output()
-            .unwrap();
-        assert_eq!(res.stdout, b"SENTINEL\n");
-    }
-
-    #[test]
-    fn sh_unset_var() {
-        assert!(bash!(r"echo $UNSETVALUE").is_err());
-    }
-
-    #[test]
-    fn sh_empty() {
-        bash!(r"true").unwrap();
-    }
-
-    #[test]
-    fn sh_empty_comma() {
-        bash!(r"true",).unwrap();
-    }
-
-    #[test]
-    fn sh_path() {
-        let p = Path::new("/no/such/path");
-        bash!(r"echo {p} >/dev/null", p = p).unwrap();
-    }
-
-    #[test]
-    fn sh_path_binary() {
-        let p = Path::new(OsStr::from_bytes(&[0x21, 0, 0xFF, 0x22, 0x61]));
-        bash!(r#"test {p} = $'!\x00\xFF\"a'"#, p = p).unwrap();
-    }
+#[test]
+fn sh_exit() {
+    assert!(bash!(r"exit {a}", a = 42).is_err())
 }
 
 #[test]
-fn cmd_rustc() {
-    let args = vec!["-V"];
-    let res = command!(
-        r"
-            rustc {args}
-        ",
-        args = args,
-    )
-    .unwrap()
-    .output()
-    .unwrap();
-    assert!(res.stdout.starts_with(b"rustc "));
+fn sh_echo1() {
+    let res = bash_command!(r"A={a}; echo $A", a = "SENTINEL")
+        .unwrap()
+        .output()
+        .unwrap();
+    assert_eq!(res.stdout, b"SENTINEL\n");
+}
+
+#[test]
+fn sh_echo2() {
+    let res = bash_command!(r"A={a}; echo $A", a = "SENTINEL",)
+        .unwrap()
+        .output()
+        .unwrap();
+    assert_eq!(res.stdout, b"SENTINEL\n");
+}
+
+#[test]
+fn sh_unset_var() {
+    assert!(bash!(r"echo $UNSETVALUE").is_err());
+}
+
+#[test]
+fn sh_empty() {
+    bash!(r"true").unwrap();
+}
+
+#[test]
+fn sh_empty_comma() {
+    bash!(r"true",).unwrap();
+}
+
+#[test]
+fn sh_path() {
+    let p = Path::new("/no/such/path");
+    bash!(r"echo {p} >/dev/null", p = p).unwrap();
+}
+
+#[test]
+fn sh_path_binary() {
+    let p = Path::new(OsStr::from_bytes(&[0x21, 0, 0xFF, 0x22, 0x61]));
+    bash!(r#"test {p} = $'!\x00\xFF\"a'"#, p = p).unwrap();
 }
